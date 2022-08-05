0a59ef3a0335d4b0fe20401da38bfda386c23d99
promisingdev72 <promising.dev72@gmail.com>
2022-08-05 11:48:10 +0300

    update

--- src/app.js
+++ src/app.js
@@ -5,9 +5,11 @@ const bodyParser = require("body-parser");
 
 const corsOptions = {
   origin: "http://localhost:3000",
+  live: "http://alitheia-studios.ch",
 };
 
 app.use(cors(corsOptions.origin));
+app.use(cors(corsOptions.live));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
